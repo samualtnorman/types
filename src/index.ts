ad682e6fff613dd3572e40d14e8ef641b31c9bea
Samual Norman <me@samual.uk>
2025-05-13 15:10:15 +0100

    add module-level jsdoc for jsr to display

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 /**
+ * # `@samual/types`
+ * This package was split from `@samual/lib` to contain the types I regularly use.
+ *
+ * [View the docs.](https://jsr.io/@samual/types/doc)
+ * @module
+ */
+
+/**
  * A better alternative to the `Function` type.
  *
  * @example
