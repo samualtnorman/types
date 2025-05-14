267ce7effcbe58ca1e2836cc96a7a99965513093
Samual Norman <me@samual.uk>
2025-05-14 10:11:07 +0100

    fix `emit-jsr-ts.js` script replacing `*/` with just ZWJ

--- scripts/emit-jsr-ts.js
+++ scripts/emit-jsr-ts.js
@@ -5,4 +5,4 @@ const readme = readFileSync(`readme.md`, { encoding: `utf8` })
 const ts = readFileSync(`src/index.ts`, { encoding: `utf8` })
 
 makeDirectorySync("dist", { recursive: true })
-writeFileSync(`dist/index.ts`, `/**\n * ${readme.trim().replaceAll(`*/`, `\u200D`).replaceAll(`\n`, `\n * `)}\n * @module\n */\n\n${ts}`)
+writeFileSync(`dist/index.ts`, `/**\n * ${readme.trim().replaceAll(`*/`, `*\u200D/`).replaceAll(`\n`, `\n * `)}\n * @module\n */\n\n${ts}`)
